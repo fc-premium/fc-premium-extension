96376769cd5516a13d06bfc6d60fa0011b6cd14c
Fernando Vega <pytness@gmail.com>
2022-05-19 16:45:19 +0200

    Handle cors requests

--- src/background.ts
+++ src/background.ts
@@ -5,6 +5,46 @@ chrome.contentSettings.javascript.set({
 	setting: 'allow'
 });
 
+
+function handleCorsRequest(details: any /* chrome.webRequest.WebRequestDetails */) {
+	if (!details.initiator.includes('forocoches.com'))
+		return;
+	
+
+	console.log(details);
+
+	const url = new URL(details.url);
+	const new_origin = url.origin;
+	// Prevent cors from being blocked
+	
+
+	let origin_is_set = false;
+
+	for (let header of details.requestHeaders) {
+		if (header.name.toLowerCase() === 'origin' || header.name.toLowerCase() === 'referer') {
+			header.value = new_origin;
+			origin_is_set = true;
+		} else if (header.name.toLowerCase().startsWith('access-control')) {
+			header.value = '*';
+		}
+	}
+
+	if (!origin_is_set) {
+		details.requestHeaders.push({
+			name: 'origin',
+			value: new_origin
+		});
+	}
+
+    return {requestHeaders: details.requestHeaders};
+}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(handleCorsRequest,
+	{urls: ["<all_urls>"]},
+	['blocking', 'requestHeaders', 'extraHeaders']
+);
+
+
 // let matches = (<any>chrome).runtime.getManifest().content_scripts[0].matches[0];
 // if (matches !== undefined) {
 //
